a8c3d4bd2ca2435212cb1a9a9bba73806dc1ea8b
Matias Mangiantini <matias.mangiantini@gmail.com>
2021-06-21 21:13:34 -0300

    Fix error message on scanner pages

--- lib/screens/scanner/widgets/scanner_results_step.dart
+++ lib/screens/scanner/widgets/scanner_results_step.dart
@@ -7,15 +7,50 @@ import 'package:yisty_app/screens/scanner/widgets/barcode_not_found.dart';
 import 'package:yisty_app/services/rest_client/api_exceptions.dart';
 import 'package:yisty_app/widgets/design/alert_page.dart';
 import 'package:yisty_app/widgets/design/loading_widget.dart';
+import 'package:yisty_app/widgets/inherited_provider.dart';
 
-class ScannerResultsStep extends StatelessWidget {
-  const ScannerResultsStep({Key key, this.barcode, this.onFinish, this.onIngredientsScan, this.productFuture, this.user}) : super(key: key);
+class ScannerResultsStep extends StatefulWidget {
+  const ScannerResultsStep({Key key, this.barcode, this.onFinish, this.onIngredientsScan, this.user}) : super(key: key);
 
-  final Future<Product> productFuture;
   final void Function() onFinish;
   final void Function() onIngredientsScan;
   final String barcode;
   final User user;
+
+  @override
+  _ScannerResultsStepState createState() => _ScannerResultsStepState();
+}
+
+class _ScannerResultsStepState extends State<ScannerResultsStep> {
+  Future<Product> future;
+
+  @override
+  void didChangeDependencies() {
+    final InheritedProvider provider = InheritedProvider.of(context);
+
+    future ??= provider.services.products.list(<String, String>{
+      'barcode': widget.barcode
+    }).then((List<Product> products) {
+      if (products.isEmpty) {
+        return null;
+      }
+
+      final Product product = products.first;
+
+      provider.services.userScans.create(
+        <String, String>{
+          'date': DateTime.now().toIso8601String(),
+          'result': product.foodPreference.toString(),
+          'productId': product.id.toString(),
+          'userId': provider.uiStore.user.id.toString()
+        }
+      );
+
+      return product;
+    });
+
+    super.didChangeDependencies();
+  }
 
   Widget _buildError(AsyncSnapshot<Product> snapshot) {
     if (snapshot.error is AppException) {
@@ -31,16 +66,23 @@ class ScannerResultsStep extends StatelessWidget {
     final Product product = snapshot.data;
 
     if (product == null) {
-      return BarcodeNotFound(barcode: barcode, onFinish: onFinish, onIngredientsScan: onIngredientsScan);
+      return BarcodeNotFound(
+        barcode: widget.barcode,
+        onFinish: widget.onFinish,
+        onIngredientsScan: widget.onIngredientsScan
+      );
     } else {
-      return BarcodeInformation(product: product, onFinish: onFinish);
+      return BarcodeInformation(
+        product: product,
+        onFinish: widget.onFinish
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<Product>(
-      future: productFuture,
+      future: future,
       builder: (_ , AsyncSnapshot<Product> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           if (snapshot.hasError) {
